0312ff2fde5785e27eda2e836626a3c9d8aa9791
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:05:30 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,35 +2,31 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Calculates the factorial of a number
-    public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
+  // Calculates the factorial of a number
+  public static double factorial(int x) {
+    // REPLACE WITH YOUR CODE HERE
+    return -1.0;
+  }
 
-    // Calculates the value for the math constant e iteratively
-    // goes until it calculates an answer less than 0.00000000001 
-    // different from the value Math.E
-    public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
+  // Calculates the value for the math constant e iteratively
+  // goes until it calculates an answer less than 0.00000000001
+  // different from the value Math.E
+  public static double calculateE() {
+    // REPLACE WITH YOUR CODE HERE
+    return -1.0;
+  }
 
-    // Calculates the square root iteratively, using the Babylonian method
-    public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
-    }
-    
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+  // Calculates the square root iteratively, using the Babylonian method
+  public static double babylonian(double x) {
+    // REPLACE WITH YOUR CODE HERE
+    return -1.0;
+  }
 
-        // YOUR CODE HERE
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
+    // YOUR CODE HERE
 
-        
-
-        scan.close();
-    }
+    scan.close();
+  }
 }
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,30 +1,52 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class MyTests {
-    @Test
-    public void testBabylonian() {
-        assertEquals(Math.sqrt(100), MyMain.babylonian(100), 0.01, "The square root of 100 should be: " + Math.sqrt(100));
-        assertEquals(Math.sqrt(50.5), MyMain.babylonian(50.5), 0.01, "The square root of 50.5 should be: " + Math.sqrt(50.5));
-        assertEquals(Math.sqrt(20.4), MyMain.babylonian(20.4), 0.01, "The square root of 20.4 should be: " + Math.sqrt(20.4));
-        assertEquals(Math.sqrt(5.2), MyMain.babylonian(5.2), 0.01, "The square root of 5.2 should be: " + Math.sqrt(5.2));
-        assertEquals(Math.sqrt(1192), MyMain.babylonian(1192), 0.01, "The square root of 1192 should be: " + Math.sqrt(1992));
-        assertEquals(Math.sqrt(17), MyMain.babylonian(17), 0.01, "The square root of 17 should be: " + Math.sqrt(17));
-    }
+  @Test
+  public void testBabylonian() {
+    assertEquals(
+        Math.sqrt(100),
+        MyMain.babylonian(100),
+        0.01,
+        "The square root of 100 should be: " + Math.sqrt(100));
+    assertEquals(
+        Math.sqrt(50.5),
+        MyMain.babylonian(50.5),
+        0.01,
+        "The square root of 50.5 should be: " + Math.sqrt(50.5));
+    assertEquals(
+        Math.sqrt(20.4),
+        MyMain.babylonian(20.4),
+        0.01,
+        "The square root of 20.4 should be: " + Math.sqrt(20.4));
+    assertEquals(
+        Math.sqrt(5.2),
+        MyMain.babylonian(5.2),
+        0.01,
+        "The square root of 5.2 should be: " + Math.sqrt(5.2));
+    assertEquals(
+        Math.sqrt(1192),
+        MyMain.babylonian(1192),
+        0.01,
+        "The square root of 1192 should be: " + Math.sqrt(1992));
+    assertEquals(
+        Math.sqrt(17),
+        MyMain.babylonian(17),
+        0.01,
+        "The square root of 17 should be: " + Math.sqrt(17));
+  }
 
-    @Test
-    public void testFactorial() {
-        assertEquals(120.0, MyMain.factorial(5), 0.01, "The factorial of 5 should be: 120.0");
-        assertEquals(3628800.0, MyMain.factorial(10), 0.01, "The factorial of 10 should be: 3628800.0");
-        assertEquals(1.0, MyMain.factorial(1), 0.01, "The factorial of 1 should be: 1.0");
-        assertEquals(1.0, MyMain.factorial(0), 0.01, "The factorial of 0 should be: 1.0");
-    }
+  @Test
+  public void testFactorial() {
+    assertEquals(120.0, MyMain.factorial(5), 0.01, "The factorial of 5 should be: 120.0");
+    assertEquals(3628800.0, MyMain.factorial(10), 0.01, "The factorial of 10 should be: 3628800.0");
+    assertEquals(1.0, MyMain.factorial(1), 0.01, "The factorial of 1 should be: 1.0");
+    assertEquals(1.0, MyMain.factorial(0), 0.01, "The factorial of 0 should be: 1.0");
+  }
 
-    @Test
-    public void testE() {
-        assertEquals(Math.E, MyMain.calculateE(), 0.01, "The value of e should be: " + Math.E);
-    }
-
-
+  @Test
+  public void testE() {
+    assertEquals(Math.E, MyMain.calculateE(), 0.01, "The value of e should be: " + Math.E);
+  }
 }
